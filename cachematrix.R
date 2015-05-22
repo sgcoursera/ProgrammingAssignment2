d7e88d4132e2b421a291f3c93e7726ee2b2e81d5
sgcoursera <jahr@bk.ru>
2015-05-22 14:26:19 +0300

    comments for code

--- cachematrix.R
+++ cachematrix.R
@@ -5,13 +5,17 @@
 ## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-        inverted <- NULL
+        inverted <- NULL   ## we will use this variable to store cached value
+	# this member function sets source matrix data
         set <- function(y) {
                 x <<- y
                 inverted <<- NULL
         }
+	# this member function is used to get source matrix data
         get <- function() x
+	# this member function is used to remember inverted matrix in inverted variable
         set_inverted <- function(inv) inverted <<- inv
+	# this member function is used to get stored value for inverted matrix
         get_inverted <- function() inverted
         list(set = set, get = get,
              set_inverted = set_inverted,
@@ -23,11 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## returned by makeCacheMatrix above. 
 
 cacheSolve <- function(x, ...) {
+	# get stored value first
         inverted <- x$get_inverted()
-        if(!is.null(inverted)) {
+        if(!is.null(inverted)) { # if stored value is not null then just return it 
                 message("getting cached data")
                 return(inverted)
         }
+	# if stored value is null, then calc it and store before exit to use it in next calls
         data <- x$get()
         inverted <- solve(data, ...)
         x$set_inverted(inverted)
